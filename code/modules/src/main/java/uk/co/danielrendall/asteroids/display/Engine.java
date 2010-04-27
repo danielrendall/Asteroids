f13385676d0e61f171487a717d5b268c6549ca18
Daniel Rendall <drendall@gmail.com>
2010-04-27 18:49:53 +0000

    Reformatted code in IntelliJ

--- code/modules/src/main/java/uk/co/danielrendall/asteroids/display/Engine.java
+++ code/modules/src/main/java/uk/co/danielrendall/asteroids/display/Engine.java
@@ -1,46 +1,50 @@
 package uk.co.danielrendall.asteroids.display;
+
 import uk.co.danielrendall.asteroids.game.*;
+
 import javax.swing.*;
 
 public final class Engine {
 
-	public final static int SCREEN_X = 600;
-	public final static int SCREEN_Y = 400;
-   private final Screen screen;
-   private final Game game;
+    public final static int SCREEN_X = 600;
+    public final static int SCREEN_Y = 400;
+    private final Screen screen;
+    private final Game game;
 
-   private Engine() {
-      screen = new Screen();
-      game = new Game();
-   }
+    private Engine() {
+        screen = new Screen();
+        game = new Game();
+    }
 
-   private static Engine instance = null;
-   public static Engine getInstance()
-   {
-      if(instance==null) { instance = new Engine(); }
-      return instance;
-   }
+    private static Engine instance = null;
 
-   public static void main(String[] args) throws Exception {
-      Engine.getInstance().run();
-   }
+    public static Engine getInstance() {
+        if (instance == null) {
+            instance = new Engine();
+        }
+        return instance;
+    }
 
-	private void run() {
-		mainLoop();
-	}
+    public static void main(String[] args) throws Exception {
+        Engine.getInstance().run();
+    }
+
+    private void run() {
+        mainLoop();
+    }
 
 
-	private void mainLoop() {
-	   System.out.println("Hello world");
-	   while (true) {
-			screen.draw(game);
-			try{
-				Thread.sleep(20);
-			}
-			catch(InterruptedException e) {
-			   e.printStackTrace();
-			}
-			game.update();
-	   }
-	}
+    private void mainLoop() {
+        System.out.println("Hello world");
+        while (true) {
+            screen.draw(game);
+            try {
+                Thread.sleep(20);
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            game.update();
+        }
+    }
 }
